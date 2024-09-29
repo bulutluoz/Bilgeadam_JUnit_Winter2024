4f8330984bb9998200996e13a5373f8d7dadeea2
bulutluoz <abbulutluoz@gmail.com>
2024-09-29 10:46:03 +0200

    Ders06 sonu

---
+++ src/test/java/day06_alerts_iFrame_multiWindows_actions/C05_KontrolluYeniWindowOlusturma.java
@@ -0,0 +1,42 @@
+package day06_alerts_iFrame_multiWindows_actions;
+
+import org.junit.jupiter.api.Test;
+import utilities.TestBase_Each;
+
+public class C05_KontrolluYeniWindowOlusturma extends TestBase_Each {
+
+    @Test
+    public void test01(){
+
+        // Test otomasyonu anasayfaya gidin
+
+        // title'in "Test Otomasyonu" icerdigini test edin
+
+
+        // Window handle degerini kaydedin ve yazdirin
+
+
+        // Yeni bir tab acarak youtube.com'a gidin
+
+
+        // url'in "youtube" icerdigini test edin
+
+
+        // Window handle degerini kaydedin ve yazdirin
+
+
+        // bagimsiz bir window olusturup bestbuy.com'a gidin
+
+
+        // Title'in "Best" icerdigini test edin
+
+
+        // Window handle degerini kaydedin ve yazdirin
+
+
+
+
+
+
+    }
+}
